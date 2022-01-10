43159465fca21e7fbda0eb68345dbbeb4983123a
TrafficConeGod <TrafficConeGod@github.com>
2022-01-10 10:21:42 -0500

    add get_unowned_array

--- src/include/array.h
+++ src/include/array.h
@@ -5,97 +5,6 @@
 #include "exception.h"
 #include "debug.h"
 
-template<typename T, std::size_t size>
-class array {
-    T data[size];
-    public:
-        array() {}
-        array(std::initializer_list<T> list) {
-            std::size_t index = 0;
-            for (const auto& elem : list) {
-                (*this)[index] = elem;
-                index++;
-            }
-        }
-
-        inline T* get_data() const { return data; }
-        inline std::size_t get_size() const { return size; }
-
-        inline T& operator[](std::size_t index) {
-            if (index >= size) {
-                dbg::print("Index out of bounds:", index, ">=", size);
-                throw_exception("Array index out of bounds");
-            }
-            return data[index];
-        }
-
-        inline const T& operator[](std::size_t index) const {
-            if (index >= size) {
-                dbg::print("Index out of bounds:", index, ">=", size);
-                throw_exception("Array index out of bounds");
-            }
-            return data[index];
-        }
-        
-        class iterator {
-            private:
-                array& array_ref;
-                size_t index;
-            public:
-                iterator(array& _array, size_t _index) : array_ref(_array), index(_index) {}
-
-                inline iterator& operator++() {
-                    index++;
-                    return *this;
-                }
-
-                inline bool operator!=(const iterator& it) const {
-                    return index != it.index;
-                }
-
-                inline T& operator*() {
-                    return array_ref[index];
-                }
-        };
-        
-        class const_iterator {
-            private:
-                const array& array_ref;
-                size_t index;
-            public:
-                const_iterator(const array& _array, size_t _index) : array_ref(_array), index(_index) {}
-
-                inline const_iterator& operator++() {
-                    index++;
-                    return *this;
-                }
-
-                inline bool operator!=(const const_iterator& it) const {
-                    return index != it.index;
-                }
-
-                inline const T& operator*() const {
-                    return array_ref[index];
-                }
-        };
-
-        inline iterator begin() {
-            return iterator(*this, 0);
-        }
-
-        inline iterator end() {
-            return iterator(*this, size);
-        }
-
-        inline const const_iterator begin() const {
-            return const_iterator(*this, 0);
-        }
-
-        inline const const_iterator end() const {
-            return const_iterator(*this, size);
-        }
-};
-
 template<typename T>
 class unowned_array {
     T* data;
@@ -189,5 +98,101 @@ class string : public unowned_array<char> {
         }
         inline operator const char*() const {
             return get_data();
+        }
+};
+
+template<typename T, std::size_t size>
+class array {
+    T data[size];
+    public:
+        array() {}
+        array(std::initializer_list<T> list) {
+            std::size_t index = 0;
+            for (const auto& elem : list) {
+                (*this)[index] = elem;
+                index++;
+            }
+        }
+        array(const array&) = delete;
+
+        inline T* get_data() const { return data; }
+        inline std::size_t get_size() const { return size; }
+
+        inline unowned_array<T> get_unowned_array() {
+            return unowned_array<T>(data, size);
+        }
+
+        inline T& operator[](std::size_t index) {
+            if (index >= size) {
+                dbg::print("Index out of bounds:", index, ">=", size);
+                throw_exception("Array index out of bounds");
+            }
+            return data[index];
+        }
+
+        inline const T& operator[](std::size_t index) const {
+            if (index >= size) {
+                dbg::print("Index out of bounds:", index, ">=", size);
+                throw_exception("Array index out of bounds");
+            }
+            return data[index];
+        }
+        
+        class iterator {
+            private:
+                array& array_ref;
+                size_t index;
+            public:
+                iterator(array& _array, size_t _index) : array_ref(_array), index(_index) {}
+
+                inline iterator& operator++() {
+                    index++;
+                    return *this;
+                }
+
+                inline bool operator!=(const iterator& it) const {
+                    return index != it.index;
+                }
+
+                inline T& operator*() {
+                    return array_ref[index];
+                }
+        };
+        
+        class const_iterator {
+            private:
+                const array& array_ref;
+                size_t index;
+            public:
+                const_iterator(const array& _array, size_t _index) : array_ref(_array), index(_index) {}
+
+                inline const_iterator& operator++() {
+                    index++;
+                    return *this;
+                }
+
+                inline bool operator!=(const const_iterator& it) const {
+                    return index != it.index;
+                }
+
+                inline const T& operator*() const {
+                    return array_ref[index];
+                }
+        };
+
+        inline iterator begin() {
+            return iterator(*this, 0);
+        }
+
+        inline iterator end() {
+            return iterator(*this, size);
+        }
+
+        inline const const_iterator begin() const {
+            return const_iterator(*this, 0);
+        }
+
+        inline const const_iterator end() const {
+            return const_iterator(*this, size);
         }
 };
