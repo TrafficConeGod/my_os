0007f34dbdb3c304e61a72a549358c99116b609e
TrafficConeGod <TrafficConeGod@github.com>
2022-01-11 16:32:59 -0500

    add array_base

--- src/include/array.h
+++ src/include/array.h
@@ -5,37 +5,39 @@
 #include "exception.h"
 #include "debug.h"
 
-template<typename T>
-class unowned_array {
-    T* data;
-    std::size_t size;
+template<typename S, typename T>
+class array_base {
+    inline T* data() const { return ((const S*)this)->base_data(); }
+    inline std::size_t size() const { return ((const S*)this)->base_size(); }
     public:
-        unowned_array(T* _data, std::size_t _size) : data(_data), size(_size) {}
+        inline T* get_data() { return data(); }
+        inline const T* get_data() const { return data(); }
+        inline std::size_t get_size() const { return size(); }
 
-        inline T* get_data() { return data; }
-        inline const T* get_data() const { return data; }
-        inline std::size_t get_size() const { return size; }
+        inline bool is_in_range(std::size_t index) {
+            return index < size();
+        }
 
         T& operator[](std::size_t index) {
-            if (index >= size) {
-                throw_index_out_of_bounds_exception(size, index);
+            if (!is_in_range(index)) {
+                // throw_index_out_of_bounds_exception(size(), index);
             }
-            return data[index];
+            return data()[index];
         }
 
         const T& operator[](std::size_t index) const {
-            if (index >= size) {
-                throw_index_out_of_bounds_exception(size, index);
+            if (!is_in_range(index)) {
+                // throw_index_out_of_bounds_exception(size(), index);
             }
-            return data[index];
+            return data()[index];
         }
         
         class iterator {
             private:
-                unowned_array& array_ref;
+                array_base& array_ref;
                 size_t index;
             public:
-                iterator(unowned_array& _array, size_t _index) : array_ref(_array), index(_index) {}
+                iterator(array_base& _array, size_t _index) : array_ref(_array), index(_index) {}
 
                 inline iterator& operator++() {
                     index++;
@@ -47,16 +49,16 @@ class unowned_array {
                 }
 
                 inline T& operator*() {
-                    return array_ref.data[index];
+                    return array_ref.data()[index];
                 }
         };
         
         class const_iterator {
             private:
-                const unowned_array& array_ref;
+                const array_base& array_ref;
                 size_t index;
             public:
-                const_iterator(const unowned_array& _array, size_t _index) : array_ref(_array), index(_index) {}
+                const_iterator(const array_base& _array, size_t _index) : array_ref(_array), index(_index) {}
 
                 inline const_iterator& operator++() {
                     index++;
@@ -68,7 +70,7 @@ class unowned_array {
                 }
 
                 inline const T& operator*() const {
-                    return array_ref.data[index];
+                    return array_ref.data()[index];
                 }
         };
 
@@ -77,7 +79,7 @@ class unowned_array {
         }
 
         inline iterator end() {
-            return iterator(*this, size);
+            return iterator(*this, size());
         }
 
         inline const const_iterator begin() const {
@@ -85,8 +87,19 @@ class unowned_array {
         }
 
         inline const const_iterator end() const {
-            return const_iterator(*this, size);
+            return const_iterator(*this, size());
         }
+};
+
+template<typename T>
+class unowned_array : public array_base<unowned_array<T>, T> {
+    T* data;
+    std::size_t size;
+    public:
+        unowned_array(T* _data, std::size_t _size) : data(_data), size(_size) {}
+
+        inline T* base_data() const { return (T*)data; }
+        inline std::size_t base_size() const { return size; }
 };
 
 class string : public unowned_array<char> {
@@ -103,7 +116,7 @@ class string : public unowned_array<char> {
 };
 
 template<typename T, std::size_t size>
-class array {
+class array : public array_base<array<T, size>, T> {
     T data[size];
     public:
         array() {}
@@ -116,83 +129,6 @@ class array {
         }
         array(const array&) = delete;
 
-        inline T* get_data() { return data; }
-        inline const T* get_data() const { return data; }
-        inline std::size_t get_size() const { return size; }
-
-        inline unowned_array<T> get_unowned_array() {
-            return unowned_array<T>(data, size);
-        }
-
-        T& operator[](std::size_t index) {
-            if (index >= size) {
-                throw_index_out_of_bounds_exception(size, index);
-            }
-            return data[index];
-        }
-
-        const T& operator[](std::size_t index) const {
-            if (index >= size) {
-                throw_index_out_of_bounds_exception(size, index);
-            }
-            return data[index];
-        }
-        
-        class iterator {
-            private:
-                array& array_ref;
-                size_t index;
-            public:
-                iterator(array& _array, size_t _index) : array_ref(_array), index(_index) {}
-
-                inline iterator& operator++() {
-                    index++;
-                    return *this;
-                }
-
-                inline bool operator!=(const iterator& it) const {
-                    return index != it.index;
-                }
-
-                inline T& operator*() {
-                    return array_ref.data[index];
-                }
-        };
-        
-        class const_iterator {
-            private:
-                const array& array_ref;
-                size_t index;
-            public:
-                const_iterator(const array& _array, size_t _index) : array_ref(_array), index(_index) {}
-
-                inline const_iterator& operator++() {
-                    index++;
-                    return *this;
-                }
-
-                inline bool operator!=(const const_iterator& it) const {
-                    return index != it.index;
-                }
-
-                inline const T& operator*() const {
-                    return array_ref.data[index];
-                }
-        };
-
-        inline iterator begin() {
-            return iterator(*this, 0);
-        }
-
-        inline iterator end() {
-            return iterator(*this, size);
-        }
-
-        inline const const_iterator begin() const {
-            return const_iterator(*this, 0);
-        }
-
-        inline const const_iterator end() const {
-            return const_iterator(*this, size);
-        }
+        inline T* base_data() const { return (T*)data; }
+        inline std::size_t base_size() const { return size; }
 };
--- src/include/dynamic_array.h
+++ src/include/dynamic_array.h
@@ -3,10 +3,11 @@
 #include <cstdlib>
 #include <cstring>
 #include <initializer_list>
+#include "array.h"
 #include "exception.h"
 
 template<typename T>
-class dynamic_array {
+class dynamic_array : public array_base<dynamic_array<T>, T> {
     T* data;
     std::size_t size;
     public:
@@ -22,26 +23,11 @@ class dynamic_array {
             std::memcpy(data, other.data, sizeof(T) * other.size);
         }
         ~dynamic_array() {
-            free(data);
+            std::free(data);
         }
 
-        inline T* get_data() { return data; }
-        inline const T* get_data() const { return data; }
-        inline std::size_t get_size() const { return size; }
-
-        T& operator[](std::size_t index) {
-            if (index >= size) {
-                throw_index_out_of_bounds_exception(size, index);
-            }
-            return data[index];
-        }
-
-        const T& operator[](std::size_t index) const {
-            if (index >= size) {
-                throw_index_out_of_bounds_exception(size, index);
-            }
-            return data[index];
-        }
+        inline T* base_data() const { return (T*)data; }
+        inline std::size_t base_size() const { return size; }
 
         void push(const T& elem) {
             size++;
@@ -54,63 +40,5 @@ class dynamic_array {
             size--;
             data = (T*)realloc(data, sizeof(T) * size);
             return elem;
-        }
-        
-        class iterator {
-            private:
-                dynamic_array& array_ref;
-                size_t index;
-            public:
-                iterator(dynamic_array& _array, size_t _index) : array_ref(_array), index(_index) {}
-
-                inline iterator& operator++() {
-                    index++;
-                    return *this;
-                }
-
-                inline bool operator!=(const iterator& it) const {
-                    return index != it.index;
-                }
-
-                inline T& operator*() {
-                    return array_ref.data[index];
-                }
-        };
-        
-        class const_iterator {
-            private:
-                const dynamic_array& array_ref;
-                size_t index;
-            public:
-                const_iterator(const dynamic_array& _array, size_t _index) : array_ref(_array), index(_index) {}
-
-                inline const_iterator& operator++() {
-                    index++;
-                    return *this;
-                }
-
-                inline bool operator!=(const const_iterator& it) const {
-                    return index != it.index;
-                }
-
-                inline const T& operator*() const {
-                    return array_ref.data[index];
-                }
-        };
-
-        inline iterator begin() {
-            return iterator(*this, 0);
-        }
-
-        inline iterator end() {
-            return iterator(*this, size);
-        }
-
-        inline const const_iterator begin() const {
-            return const_iterator(*this, 0);
-        }
-
-        inline const const_iterator end() const {
-            return const_iterator(*this, size);
         }
 };
